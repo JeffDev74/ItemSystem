489b93fa471544b31e5f5f8dd6841e58ebfc3db7
JeffDev74 <jeffdev74@gmail.com>
2017-04-12 22:30:47 -0700

    Implemented int custom property

--- Assets/JeffDev#ItemSystem/Items/CustomProperties/Properties/IntProperty.cs
+++ Assets/JeffDev#ItemSystem/Items/CustomProperties/Properties/IntProperty.cs
@@ -1,9 +1,20 @@
 ﻿using UnityEngine;
 
-namespace FPS
+namespace FPS.ItemSystem.CustomProperty
 {
-	public class IntProperty : MonoBehaviour
-	{
+    [System.Serializable]
+    public class IntProperty : Property, IProperty
+    {
+        public IntProperty(string key, object value) : base(key, value) { }
 
-	}
+        public override T Deserialize<T>()
+        {
+            return (T)System.Convert.ChangeType(Value, typeof(T));
+        }
+
+        public override string Serialize(object obj)
+        {
+            return obj.ToString();
+        }
+    }
 }
